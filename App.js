d8b6192a56b3e09fce4babe64d88d734577de215
melanya98 <106449920+melanya98@users.noreply.github.com>
2022-07-01 14:25:43 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
 import "./App.css"
-import React from "react"
-import { ReactDOM } from "react" 
 import { useState } from "react"
-
-const options = [
-  { id: 1, value: 5, text: "5x5" },
-  { id: 2, value: 7, text: "7x7" },
-  { id: 3, value: 10, text: "10x10" },
-]
-
+let count = 0
 const FREE_CELL = 0
 const RABBIT = "Rabbit"
 const WOLF = "wOLF"
 const FENCE = 3
 const HOME = "Home"
 
-// const X = 0
-// const Y = 1
-
-const ValueSizeArray = [5, 7, 10]
-
-let boardNumber = 0
+const X = 0
+const Y = 1
 
 const IMAGES = {
   [RABBIT]: "img/rabbit.gif",
@@ -28,45 +16,26 @@ const IMAGES = {
   [FENCE]: "img/ban.png",
   [HOME]: "img/home.png",
 }
-
-function Image(props) {
-  if (props.character === RABBIT) {
-    const image = IMAGES[RABBIT]
-    return <img src={image} />
+function App() {
+  count++
+  const [gameBoardNumber, setGamboardNumber] = useState([])
+  function clickButton() {
+    setGamboardNumber([...gameBoardNumber, count])
+    console.log("aaa")
   }
-  if (props.character === WOLF) {
-    const image = IMAGES[WOLF]
-    return <img src={image} />
-  }
-  if (props.character === FENCE) {
-    const image = IMAGES[FENCE]
-    return <img src={image} />
-  }
-  if (props.character === HOME) {
-    const image = IMAGES[HOME]
-    return <img src={image} />
-  }
-}
-function findCordinateCharacter(array, character) {
-  const findeMtrix = function(accumulator, row, x) {
-    row.forEach((cell, y) => {
-      if (cell === character) {
-        accumulator.push([x, y])
-      }
-    })
-    return accumulator
-  }
-  return array.reduce(findeMtrix, [])
+  return (
+    <div className="App">
+      {gameBoardNumber.map((number) => {
+        return <StartNewGame key={number} />
+      })}
+      <button id="newBoardButton" onClick={clickButton}>
+        New Board
+      </button>
+    </div>
+  )
 }
 
-function changeGameStatus(gameState, status) {
-  gameState.gameOver = true
-  gameState.gameResult = status
-  console.log(status)
-}
-
-const newGame = ReactDOM.createRoot(document.getElementById("newGame"))
-const container = ReactDOM.createRoot(document.getElementById("container"))
+export default App
 
 function CreateMatrix(value) {
   let arr = []
@@ -119,78 +88,29 @@ function DefineRandomPosition(gameState, character) {
   }
 }
 
-function rabbitGoTo(gameState, rabbitIndex, x, y) {
-  const [i, j] = rabbitIndex
-
-  if (gameState[x][y] === FREE_CELL) {
-    gameState[x][y] = RABBIT
-    gameState[i][j] = FREE_CELL
-    console.log(gameState)
+function Image(props) {
+  if (props.character === RABBIT) {
+    const image = IMAGES[RABBIT]
+    return <img src={image} alt="#" />
   }
-  if (gameState[x][y] === HOME) {
-    changeGameStatus(gameState, "win")
-    return
+  if (props.character === WOLF) {
+    const image = IMAGES[WOLF]
+    return <img src={image} alt="#" />
   }
-
-  if (gameState[x][y] === WOLF) {
-    changeGameStatus(gameState, "gameOver")
-    return
+  if (props.character === FENCE) {
+    const image = IMAGES[FENCE]
+    return <img src={image} alt="#" />
   }
-}
-
-function getDirectionCoord(gameMatrix, rabbitIndex, eventKey) {
-  let [x, y] = rabbitIndex
-  let newX = x
-  let newY = y
-  const arraySize = gameMatrix.length - 1
-  if (eventKey === "ArrowRight") {
-    newY = y + 1
-    if (y === arraySize) {
-      newY = 0
-    }
-  } else if (eventKey === "ArrowLeft") {
-    newY = y - 1
-    if (y === 0) {
-      newY = arraySize
-    }
-  } else if (eventKey === "ArrowUp") {
-    newX = x - 1
-    if (x === 0) {
-      newX = arraySize
-    }
-  } else if (eventKey === "ArrowDown") {
-    newX = x + 1
-    if (x === arraySize) {
-      newX = 0
-    }
+  if (props.character === HOME) {
+    const image = IMAGES[HOME]
+    return <img src={image} alt="#" />
   }
-  return [newX, newY]
-}
-function EventKeysFunctions(props) {
-  const gameMatrix = props.array.gameMatrix
-  console.log(gameMatrix)
-  const key = props.eventKey
-  const rabbitIndex = findCordinateCharacter(gameMatrix, RABBIT)[0]
-  const [newX, newY] = getDirectionCoord(gameMatrix, rabbitIndex, key)
-  rabbitGoTo(gameMatrix, rabbitIndex, newX, newY)
-  const [x, y] = rabbitIndex
-
-  return gameMatrix
 }
 
 function BtnEvent(props) {
-  const gameState = props.state.gameMatrix
   return (
     <div id="btnEvent" className="btnEvent">
-      <button
-        id="up"
-        className="up"
-        onClick={() => {
-          props.newSituation(
-            <EventKeysFunctions array={gameState} eventKey="up" />
-          )
-        }}
-      >
+      <button id="up" className="up">
         Up
       </button>
       <button id="left" className="left">
@@ -207,99 +127,60 @@ function BtnEvent(props) {
 }
 
 function StartNewGame() {
-  let select = document.getElementById("select")
-  const value = select.value
-  const styleObject = {
-    width: value * 80 + 100 + "px",
-  }
-  const array = CreateMatrix(value)
-
-  const gameState = {
-    gameMatrix: array,
+  const optionsArray = [5, 7, 10]
+  const [value, setValue] = useState(optionsArray[0])
+  const [gameState, setGameState] = useState({
+    gameMatrix: [],
     gameOver: false,
-    gameResult: "",
-    gamNumber: boardNumber,
+    gameStatus: "",
+  })
+  console.log(gameState)
+  const handleChange = (event) => setValue(event.target.value)
+  const styleObject = {
+    width: gameState.gameMatrix.length * 80 + 100 + "px",
   }
-  DefineAtributes(gameState, value)
-  const gameArray = gameState.gameMatrix
-  const [now, setNew] = useState(gameState)
-  return (
-    <div id="newGameArea" className="newGameArea" style={styleObject}>
-      {gameArray.map((row, x) => {
-        return row.map((column, y) => {
-          return (
-            <div className="box" key={x.toString() + y.toString()}>
-              <Image character={column} />
-            </div>
-          )
-        })
-      })}
-      <BtnEvent state={gameState} newSituation={setNew} />
-    </div>
-  )
-}
 
-function SelectAndStartButton() {
-  const handleClick = () => {
-    // ;<StartNewGame />
+  console.log(gameState)
+  function handleClick() {
+    setGameState({
+      gameMatrix: CreateMatrix(value),
+      gameOver: false,
+      gameResult: "",
+    })
   }
+
+
+  const gameArray = gameState.gameMatrix
+  console.log(gameState)
+
   return (
     <div>
       <button id="start" className="start" onClick={handleClick}>
         Start
       </button>
-      <select className="select" id="select">
-        {options.map((item) => {
+
+      <select className="select" id="select" onChange={handleChange}>
+        {optionsArray.map((option) => {
           return (
-            <option key={item.value} value={item.value}>
-              {item.text}
+            <option key={option} value={option}>
+              {option}X{option}
             </option>
           )
         })}
       </select>
+      <div id="newGameArea" className="newGameArea" style={styleObject}>
+        {gameArray.map((row, x) => {
+          return row.map((column, y) => {
+            return (
+              <div className="box" key={x.toString() + y.toString()}>
+                <Image character={column} />
+              </div>
+            )
+          })
+        })}
+        <BtnEvent />
+ 
+      </div>
     </div>
   )
 }
-
-// function SelectAndStartButton() {
-//   return (
-//     <div>
-//       <button
-//         id="start"
-//         className="start"
-//         onClick={(eventt) => {
-//           newGame.render(<StartNewGame />)
-//         }}
-//       >
-//         Start
-//       </button>
-//       <select className="select" id="select">
-//         {options.map((item) => {
-//           return (
-//             <option key={item.value} value={item.value}>
-//               {item.text}
-//             </option>
-//           )
-//         })}
-//       </select>
-//     </div>
-//   )
-// }
-
-function App() {
-  return (
-    <div className={"App"}>
-      <button
-        id="newBoardButton"
-        onClick={(evt) => {
-          {
-            container.render(<SelectAndStartButton />)
-          }
-        }}
-      >
-        New Board
-      </button>
-    </div>
-  )
-}
-export default App
